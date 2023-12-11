6b42b493210d0bd454bf18f5f428da2d71e2c02f
vsmm-world <thekingjarvis792@gmail.com>
2023-12-11 20:14:17 +0530

    variable and data types

--- basics/variable.js
+++ basics/variable.js
@@ -8,6 +8,12 @@ let name="Ravindra Valand"; //string
 let counts = [1,2,3,4,5]; //array
 let info = {name:"Ravindra",age:23}; //object
 
+{
+    let x = 5;
+    var y = 10;
+}
+console.log(y); // Output: 10
+console.log(x); // Error: 'x' is not defined (due to block-level scope of let)
 
 
 
