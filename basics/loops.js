343d22a5ef16cb87569f7428f290ccf5150b2e4d
vsmm-world <thekingjarvis792@gmail.com>
2023-12-11 20:22:06 +0530

    loops in nodejs

--- basics/loops.js
+++ basics/loops.js
@@ -13,3 +13,18 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     //code to be executed
 }
+
+array.forEach(element => {
+    //code to be executed
+});
+
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        //code to be executed
+     }
+}
+
+for (const iterator of object) {
+    //code to be executed
+}
