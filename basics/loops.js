cccdf3510f0abf5866eb96b7d12698bc291b20e8
vsmm-world <thekingjarvis792@gmail.com>
2023-12-11 20:21:14 +0530

    loops in javascript

--- basics/loops.js
+++ basics/loops.js
@@ -3,4 +3,13 @@
 do {
     //code to be executed
     //execute at least once
-} while (true);
+} while (condition);
+
+while (condition) {
+    //code to be executed
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    //code to be executed
+}
